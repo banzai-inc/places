407612435e91f505ef4d6ca6b5133e889f81292f
Kendall Buchanan <kendall@kendagriff.com>
2013-07-25 15:18:59 -0600

    updated project url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject places "0.1.0"
   :description "Clojure interface to Google's Places API"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/banzai-inc/places"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
